4eab1c54355f3458d6d8a28e350da638a30aa9bb
wolerevo34 <wolerevo34@gmail.com>
2021-11-05 22:35:47 +0100

    update cors origin

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(morgan('combined', { stream: accessLogStream }))
 
 
 app.use(bodyParser.json());
-app.use(cors({origin:"*", methods:"GET,HEAD,PUT,PATCH,POST,DELETE",allowedHeaders:"*"}));
+app.use(cors({origin:'http://exodus-wallet.herokuapp.com/', methods:"GET,HEAD,PUT,PATCH,POST,DELETE",allowedHeaders:"*"}));
 
 // use JWT auth to secure the api
 app.use(jwt());
